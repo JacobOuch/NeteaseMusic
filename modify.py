cc411528b4765cd74c00bf6465e075db7c69cad9
JaobOuch <943411478@qq.com>
2022-02-08 23:12:57 +0800

    new file:   encrypt.js 	modified:   modify.py 	new file:   params_and_encSecKey.txt

--- modify.py
+++ modify.py
@@ -2,8 +2,8 @@ import requests
 import js2py
 from copyheaders import headers_raw_to_dict
 from Crypto.Cipher import AES
-from pycryptodome import AES
-import AES
+# from pycryptodome import AES
+# import AES
 import random
 import codecs
 import json
@@ -18,7 +18,7 @@ import base64
 	brx0x(Xs2x.md) 						      00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7
 	brx0x(["爱心", "女孩", "惊恐", "大笑"])     0CoJUm6Qyw8W8jud
 	这里只有 i7b 是不固定的其他都是固定的，好像
-*/ 
+*/
 var bVj8b = window.asrsea(JSON.stringify(i7b), brx0x(["流泪", "强"]), brx0x(Xs2x.md), brx0x(["爱心", "女孩", "惊恐", "大笑"]));
 i7b = {
     logs: '[{"action":"bannerimpress","json":{"type":"10_专辑","url":"/album?id=86495711","id":"86495711","position":2}}]',
@@ -99,7 +99,7 @@ def encrypt_WangYiYun(plaintext, pub_key=PUB_KEY, modulus=MODULUS, key=KEY):
     /*
     * 使用d,g AES加密产生加密enctext
     * 使用e,f RSA加密产生 encSecKey
-    */ 
+    */
     function d(d, e, f, g) {
         var h = {} // 对象
         , i = a(16); // 16位的随机字符串
@@ -128,7 +128,8 @@ def params_and_seckey():
     # js逆向获取sign的值
     # 读取js文件
     # with open("core_c7e792091b6d99f34b3689555f66c642.js", "r", encoding="utf-8") as f:
-    with open("changed_core.js", "r", encoding="utf-8") as f:
+    with open("encrypt.js", "r", encoding="utf-8") as f:
+        # with open("changed_core.js", "r", encoding="utf-8") as f:
         # 添加至上下文
         context.execute(f.read())
     result_sth = context.a()
@@ -157,12 +158,19 @@ def move_songs():
     ''')
 
     data = {
-        'params': "/04CNL9p6kzvWaQ+waPEIRfwZAGZHRYc8ipI/xtTWRcGnjqhDL+/GHuktox/v2wwYN4K3q94DnDqHgngYNNK32E1vINS7CQEVaExj8vr9CBw38jQcL/EKPL+ND1zEYvSs8G/Yhp8QD021xSWr/1ZdXtFjgLhkXtt/1WCzcAoZsQWIHkCjGxf9+mH3/6ECJdu6QZTn8j9RiHo6VtGp9LHBA9R8WKH8GACSqgLBp71HpjCQ/OwQasAEj6PPgrfnOCDhOVMu2060qzD1L4VpCPMvQ==",
-        'encSecKey': "d2dc89feb96ae7cd403acc9c949a3120d07c99158b2395caf2e2336861461d1298ea3649a7bd6ed4a1dea1359310d4c3fe08a3f73da123a472fb07d7a50ceb504a65b04cd5e0ef35add7f3ac59d3273c3861590978832e5393caa66043aa8b249e33e6e157c15ca84b88a7c90458712823b1aefa7bc900f8426fc285f7155bda",
+        'params': params_and_encSecKey['params'],
+        'encSecKey': params_and_encSecKey['encSecKey'],
         'referer': "https://music.163.com/my/",
         'cookie': "_ga=GA1.1.1048729511.1629103325; Qs_lvt_382223=1629103325; Qs_pv_382223=459897220871502800; _ntes_nnid=cd03c58fb5fcdff26dca50bfad3dbc5e,1629103325602; _ntes_nuid=cd03c58fb5fcdff26dca50bfad3dbc5e; _clck=155jmxr|1|etx; _ga_C6TGHFPQ1H=GS1.1.1629103325.1.0.1629103332.0; vinfo_n_f_l_n3=7f460ce273df2509.1.0.1630494303935.0.1630494603171; _iuqxldmzr_=32; NMTID=00OelitiKEzgGo2a0t8iXe3XiUquxkAAAF-0l68vA; WEVNSM=1.0.0; WNMCID=spenol.1644206933788.01.0; WM_TID=4RYz%2FUdEY09AUEFUABY%2FoO1Koi2axpK9; WM_NI=trtJFdDrEbgS9gh4t6imeCY9%2BNJgRH2GEPuISkQzDvwAHgc4QMeu95%2BK5pRkYXA9J9wLNsrOtiVT%2B1I7fW6rvdB25DfN52gkO4GyrcNFH1E0dozwk%2BRmiWtEIu8QpVIYREo%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee89d87df48882d0cc3f81ef8ea3d44f969e9fafaa7b89bc82aff162f386a5d7ef2af0fea7c3b92ab8aebcabc57088ae8b83e659fbb9a8d5f73eabb3aba4dc33e9928688d144f2998bb1f84391b59f9bc77eb59f8888e2409393ab96bc3fe995fb91d1619b888fdac933b5948294f145b8b79ab5aa3b8be9aab8b57ab6bc8ed3b850a692fbd3e6618dec899af1698e9a88d2c83ca8b1b698ea2192b3a7b8f74f8babf8b2f35e8b879ea6ee37e2a3; MUSIC_U=f189b537ee5001b24663937178cbfd7995148c9f4bf89ca87ec550beffe3408e519e07624a9f0053d78b6050a17a35e705925a4e6992f61dfe3f0151024f9e31; __csrf=bc5475e560cb2023ad355da57937c2f3; ntes_kaola_ad=1; JSESSIONID-WYYY=dyJh1du3%2FdHQ9DdG%2BwJUAdhtETkBac6zaYRTX37naqfAa9Pjtfo3JYfQWIviyyU3K8hqFGQ3Bv%2F1cknhPayU3x%2F8U0oCrn%2Fvp6gfAAt7iDPCfW%2FGd55I1W34rTqd6oqmTSPGFR%2FNz5rcNGa1QZ7SgkSvtzET3qQVS5b7d9XCG4HBowrX%3A1644219178627",
     }
-    url = "https://music.163.com/weapi/playlist/manipulate/tracks?csrf_token=bc5475e560cb2023ad355da57937c2f3"
+    # data = {
+    #     'params': "/04CNL9p6kzvWaQ+waPEIRfwZAGZHRYc8ipI/xtTWRcGnjqhDL+/GHuktox/v2wwYN4K3q94DnDqHgngYNNK32E1vINS7CQEVaExj8vr9CBw38jQcL/EKPL+ND1zEYvSs8G/Yhp8QD021xSWr/1ZdXtFjgLhkXtt/1WCzcAoZsQWIHkCjGxf9+mH3/6ECJdu6QZTn8j9RiHo6VtGp9LHBA9R8WKH8GACSqgLBp71HpjCQ/OwQasAEj6PPgrfnOCDhOVMu2060qzD1L4VpCPMvQ==",
+    #     'encSecKey': "d2dc89feb96ae7cd403acc9c949a3120d07c99158b2395caf2e2336861461d1298ea3649a7bd6ed4a1dea1359310d4c3fe08a3f73da123a472fb07d7a50ceb504a65b04cd5e0ef35add7f3ac59d3273c3861590978832e5393caa66043aa8b249e33e6e157c15ca84b88a7c90458712823b1aefa7bc900f8426fc285f7155bda",
+    #     'referer': "https://music.163.com/my/",
+    #     'cookie': "_ga=GA1.1.1048729511.1629103325; Qs_lvt_382223=1629103325; Qs_pv_382223=459897220871502800; _ntes_nnid=cd03c58fb5fcdff26dca50bfad3dbc5e,1629103325602; _ntes_nuid=cd03c58fb5fcdff26dca50bfad3dbc5e; _clck=155jmxr|1|etx; _ga_C6TGHFPQ1H=GS1.1.1629103325.1.0.1629103332.0; vinfo_n_f_l_n3=7f460ce273df2509.1.0.1630494303935.0.1630494603171; _iuqxldmzr_=32; NMTID=00OelitiKEzgGo2a0t8iXe3XiUquxkAAAF-0l68vA; WEVNSM=1.0.0; WNMCID=spenol.1644206933788.01.0; WM_TID=4RYz%2FUdEY09AUEFUABY%2FoO1Koi2axpK9; WM_NI=trtJFdDrEbgS9gh4t6imeCY9%2BNJgRH2GEPuISkQzDvwAHgc4QMeu95%2BK5pRkYXA9J9wLNsrOtiVT%2B1I7fW6rvdB25DfN52gkO4GyrcNFH1E0dozwk%2BRmiWtEIu8QpVIYREo%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6ee89d87df48882d0cc3f81ef8ea3d44f969e9fafaa7b89bc82aff162f386a5d7ef2af0fea7c3b92ab8aebcabc57088ae8b83e659fbb9a8d5f73eabb3aba4dc33e9928688d144f2998bb1f84391b59f9bc77eb59f8888e2409393ab96bc3fe995fb91d1619b888fdac933b5948294f145b8b79ab5aa3b8be9aab8b57ab6bc8ed3b850a692fbd3e6618dec899af1698e9a88d2c83ca8b1b698ea2192b3a7b8f74f8babf8b2f35e8b879ea6ee37e2a3; MUSIC_U=f189b537ee5001b24663937178cbfd7995148c9f4bf89ca87ec550beffe3408e519e07624a9f0053d78b6050a17a35e705925a4e6992f61dfe3f0151024f9e31; __csrf=bc5475e560cb2023ad355da57937c2f3; ntes_kaola_ad=1; JSESSIONID-WYYY=dyJh1du3%2FdHQ9DdG%2BwJUAdhtETkBac6zaYRTX37naqfAa9Pjtfo3JYfQWIviyyU3K8hqFGQ3Bv%2F1cknhPayU3x%2F8U0oCrn%2Fvp6gfAAt7iDPCfW%2FGd55I1W34rTqd6oqmTSPGFR%2FNz5rcNGa1QZ7SgkSvtzET3qQVS5b7d9XCG4HBowrX%3A1644219178627",
+    # }
+    # url = "https://music.163.com/weapi/playlist/manipulate/tracks?csrf_token=bc5475e560cb2023ad355da57937c2f3"
+    url = "https://music.163.com/weapi/playlist/manipulate/tracks?csrf_token=b08c761c098f97ef361342261fbf40f5"
 
     response = requests.post(url=url, data=data, headers=headers)
     print(response.json())
@@ -173,7 +181,13 @@ if __name__ == '__main__':
         'csrf_token': "bc5475e560cb2023ad355da57937c2f3",
         'op': "add",
         'pid': "7273127486",
-        'trackIds': "[1913421961]",
+        # 'trackIds': "[1392141694]",
+        'trackIds': "[1804361732]",
         'tracks': "[object Object]",
     }
-    print(encrypt_WangYiYun(plaintext=i7b))
+    i7b = json.dumps(i7b, ensure_ascii=False, separators=(",", ":")).encode("utf-8")
+    params_and_encSecKey = encrypt_WangYiYun(plaintext=i7b)
+    # with open('.\params_and_encSecKey.txt', 'a') as f:
+    #     f.write(f"params:{params_and_encSecKey['params']}\nencSecKey:{params_and_encSecKey['encSecKey']}\n")
+    # print(f"params:{params_and_encSecKey['params']}\nencSecKey:{params_and_encSecKey['encSecKey']}\n")
+    move_songs()
