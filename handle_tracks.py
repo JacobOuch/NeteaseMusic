cd081f37ac348b7d0d678d3c84f3fd507fa524be
JaobOuch <943411478@qq.com>
2022-02-10 23:05:28 +0800

    modified:   README.md 	new file:   __pycache__/encrypt.cpython-310.pyc 	modified:   __pycache__/handle_tracks.cpython-310.pyc 	deleted:    core_c7e792091b6d99f34b3689555f66c642.js 	new file:   encrypt.py 	modified:   handle_tracks.py 	deleted:    modify.py 	new file:   playlists.py

--- handle_tracks.py
+++ handle_tracks.py
@@ -21,7 +21,7 @@ import pandas as pd
 #     df.to_csv('.\changed_唔係咁打嘅喜欢的音乐.csv', sep=',', encoding='utf-8')
 #     return english_ids
 
-def get_ids():
+def get_ids(playlist):
     """
         返回歌曲id列表，函数中的language（小写）用来存储对应歌曲的语言
     :return: ids字典，其中 english_ids存放英文歌的列表，chinese_ids存放中文歌的列表，japanese_ids存放日文歌的列表
@@ -30,17 +30,19 @@ def get_ids():
     ch_pattern = re.compile(r'[\u4E00-\u9FA5|\u4E00-\u9FFF]+')     # 匹配中文的模式
     jp_pattern = re.compile(r'[\u3040-\u309f|\u30a0-\u30ff]+')      # 匹配日文
     kr_pattern = re.compile(r'[\uac00-\ud7ff]+')      # 匹配韩文
-    df = pd.read_csv('唔係咁打嘅喜欢的音乐.csv',
+    df = pd.read_csv(f'{playlist}.csv',
                      sep=',',
-                     encoding='utf-8')  # filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，header=None表示头部为空，sep=' '表示数据间使用空格作为分隔符，如果分隔符是逗号，只需换成 ‘，’即可。
+                     encoding='utf-8')  # filename可以直接从盘符开始，标明每一级的文件夹直到csv文件，header=None表示头部为空，
+    # sep=' '表示数据间使用空格作为分隔符，如果分隔符是逗号，只需换成 ‘，’即可。
     del df['Unnamed: 0']  # 删除Unnamed列
     language, ids = list(), dict()
     ids['english_ids'], ids['chinese_ids'], ids['japanese_ids'], ids['korean_ids'] = [], [], [], []
     for track in df.iterrows():
         if re.match(en_pattern, track[1]['names']) and re.match(en_pattern, track[1]['singers']):
+            # 用match是因为大部分英文歌曲的歌手名和歌曲名都应该匹配英文模式
             language.append('en')
             ids['english_ids'].append(track[1]['ids'])
-        # 日语优先级比中文高，因为日语中可能有中文
+        # 日语优先级比中文高，因为日语中可能有简体中文，中日韩文均是可以用search来匹配，只要搜到对应的文字就可以当成是该种语言的歌曲
         elif re.search(jp_pattern, track[1]['names']) or re.search(jp_pattern, track[1]['singers']):
             language.append('jp')
             ids['japanese_ids'].append(track[1]['ids'])
@@ -53,9 +55,9 @@ def get_ids():
         else:
             language.append('unknown')
 
-    df['Language'] = language
+    df['Language'] = language   # 新增一列保存歌曲的语言
     # print(df.head(7))
-    df.to_csv('.\changed_唔係咁打嘅喜欢的音乐.csv', sep=',', encoding='utf-8')
+    df.to_csv(f'.\changed_{playlist}.csv', sep=',', encoding='utf-8')
     return ids
 
 
